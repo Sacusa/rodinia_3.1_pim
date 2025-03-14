8eb967d09e99eee1e81daabaceb211a436cba5ee
Sacusa <sudhanshugupta96@gmail.com>
2025-03-14 16:47:23 -0400

    Rename PIM/MEM launch script

---
+++ cuda/launch_all_pim_mem.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+source common.sh
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <policy> [policy parameters]"
+    exit
+fi
+
+declare -a pim_apps=("stream_copy" "stream_daxpy" "stream_scale" "bn_fwd"
+    "bn_bwd" "fully_connected" "kmeans" "histogram" "grim")
+
+policy_out_dir=$(set_policy_in_config "$@")
+
+if [[ $? -ne 0 ]]; then
+    exit
+fi
+
+output_dir=output/${policy_out_dir}
+mkdir -p ${output_dir}
+
+max_concurrent_apps=$((`nproc` / 2))
+num_concurrent_apps=0
+
+for pim_app in "${pim_apps[@]}"; do
+    for mem_app in "${mem_apps[@]}"; do
+        ./launch_mem.sh ${pim_app} ${mem_app} &> \
+            ${output_dir}/${mem_app}_${pim_app} &
+
+        ((num_concurrent_apps++))
+
+        if (( num_concurrent_apps == max_concurrent_apps )); then
+            num_concurrent_apps=0
+            wait
+        fi
+    done
+done
+
+wait
